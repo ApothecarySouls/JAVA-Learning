f365877405aebac638d15c9ab0e9278f6462d103
Михаил Ларионов <Bishop@Mihails-MacBook-Pro.local>
2019-04-07 19:30:20 +0200

    add Java file

---
+++ src/Array_of_Arrays.java
@@ -0,0 +1,10 @@
+public class Array_of_Arrays {
+
+    public static void main(String[] args) {
+    String[][] birdsBase = new String[5][];
+    }
+    public void createBird()
+    {
+        String[] newBird = new String[];
+    }
+}
